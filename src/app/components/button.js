610067892a0c0c3f01fde29e871fa55dcd2ee3c7
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-12-10 19:44:54 +0530

    prop classname and onclick added

--- src/app/components/button.js
+++ src/app/components/button.js
@@ -3,8 +3,8 @@ const Button = (props) => {
     const btnStyle = "px-10 py-4 border-2 border-primary rounded-full lg:hover:scale-105 transition-all ease-in-out";
     return (
         <button
-            className={`${btnStyle} ${props.variant !== 'outline' ? filledVariantStyle : ''}`}
-        // onClick={props.handleClick}
+            className={`${btnStyle} ${props.variant !== 'outline' ? filledVariantStyle : ''} ${props.className}`}
+            onClick={props.handleClick}
         >
             {props.text}
         </button>
