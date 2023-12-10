81925c69758abd85994157df11d264a2a62eba53
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-12-10 20:16:56 +0530

    structural and prop changes

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -2,13 +2,13 @@ import Image from "next/image"
 
 const Card = (props) => {
     return (
-        <div className="flex flex-col">
+        <a href="#" className="flex flex-col">
             <div className="relative w-full h-full min-h-[12rem] lg:min-h-[20rem] rounded-sm overflow-hidden mb-2">
                 <Image src={props.image} alt="food" objectFit="cover" layout="fill" />
             </div>
-            {props.title && <p className="text-lg font-semibold">{props.title}</p>}
-            {props.subtitle && <p className="text-sm">{props.subtitle}</p>}
-        </div>
+            {props.name && <p className="text-lg font-semibold">{props.name}</p>}
+            {props.desc && <p className="text-sm">{props.desc}</p>}
+        </a>
     )
 }
 
