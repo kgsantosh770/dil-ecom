fab3698dc8c2a55d1be7ab02392802ec4a038621
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-12-12 17:21:58 +0530

    input field non empty validation

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,15 +1,32 @@
 'use client';
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 import Input from "../_components/input"
-import Link from "next/link";
 import Button from "../_components/button";
 
 const Payment = () => {
     const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
     const currentYear = new Date().getFullYear();
     const next10Years = Array.from({ length: 20 }, (_, index) => currentYear + index + 1);
+    const cardNameRef = useRef(null);
+    const cardNumberRef = useRef(null);
+    const cardCvvRef = useRef(null);
+    const [error, setError] = useState(false);
+    const router = useRouter();
 
-    return (
+    const handlePay = () => {
+        setError(false);
+        const cardName = cardNameRef.current?.value;
+        const cardNumber = cardNumberRef.current?.value;
+        const cardCvv = cardCvvRef.current?.value;
+        if(cardName && cardNumber && cardCvv) {
+            setError(false);
+            router.push('/payment/success');
+        }
+        else setError(true);
+    }
+
+    return router ? (
         <div className='px-4 py-6 lg:max-w-xl lg:mx-auto'>
             <h2 className='text-2xl font-bold'>Checkout</h2>
             <div className='flex items-center gap-3 mt-3'>
@@ -21,19 +38,18 @@ const Payment = () => {
             </div>
             <p className='text-xl mt-10'>Payment Details</p>
             <form className="mt-4 flex flex-col gap-4">
-                <Input className='w-full' type="text" placeholder="Card Holder Name" />
-                <Input className='w-full' type="tel" maxLength={12} placeholder="Card Number" />
+                <Input innerRef={cardNameRef} className='w-full' type="text" placeholder="Card Holder Name" />
+                <Input innerRef={cardNumberRef} className='w-full' type="tel" maxLength={12} placeholder="Card Number" />
                 <div className="flex gap-3 justify-between">
                     <Input className='w-full' type="dropdown" placeholder="Month" options={months} />
                     <Input className='w-full' type="dropdown" placeholder="Year" options={next10Years} />
                 </div>
-                <Input className="w-full" type="tel" maxLength={3} placeholder="CVV" />
+                <Input innerRef={cardCvvRef} className="w-full" type="tel" maxLength={3} placeholder="CVV" />
             </form>
-            <Link href="/payment/success">
-                <Button text="Pay" className="block mx-auto mt-8 w-full lg:max-w-sm"/>
-            </Link>
+            {error && <p className="font-bold mt-4 text-md text-primary text-center">Please fill in the details.</p>}
+            <Button onClick={()=>handlePay()} text="Pay" className="block mx-auto mt-8 w-full lg:max-w-sm" />
         </div>
-    )
+    ): 'Loading';
 }
 
 export default Payment
