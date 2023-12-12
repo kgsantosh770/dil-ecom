eae2b26c0a9b720b0b1a2b702b4a589eaa9df32f
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-12-12 16:45:00 +0530

    payment confirmation page

---
+++ src/app/payment/success/page.js
@@ -0,0 +1,17 @@
+import Button from "@/app/_components/button"
+import Link from "next/link"
+
+const PaymentSuccess = () => {
+  return (
+    <div className="mt-[20%] px-4 text-center">
+        <h1 className="text-3xl font-bold">Payment Success ! </h1>
+        <p className="text-6xl mt-4">🎉</p>
+        <p className="mt-10">Your order will arrive soon</p>
+        <Link href='/'>
+            <Button className="mt-6" text="Shop more" />
+        </Link>
+    </div>
+  )
+}
+
+export default PaymentSuccess
